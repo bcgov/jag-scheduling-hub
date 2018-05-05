428f9c32be095a2810ff367184942b181ff2d4ec
Eric Mignot <eric.mignot@gmail.com>
2018-05-04 17:31:40 -0700

    ExtractClass RouteTest to remove noise from PingTest

--- src/test/java/hub/PingTest.java
+++ src/test/java/hub/PingTest.java
@@ -1,42 +1,23 @@
 package hub;
 
 import hub.support.Resource;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.junit.After;
+import hub.support.RouteTest;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class PingTest {
-
-    protected Server server;
+public class PingTest extends RouteTest {
 
     @Before
-    public void aJettyServer() throws Exception {
-        server = new Server(8888);
-    }
-    @After
-    public void stopServer() throws Exception {
-        server.stop();
-    }
-
-    @Before
-    public void setProperties() {
-        System.setProperty("COA_SEARCH_ENDPOINT", "http4://localhost:8111");
+    public void setCommitHash() {
         System.setProperty("OPENSHIFT_BUILD_COMMIT", "42isTheAnswer");
     }
 
     @Test
     public void returnsCommitHash() throws Exception {
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        context.setResourceBase("src/main/resources");
         context.addServlet(PingServlet.class, "/ping");
-        context.addEventListener(new org.jboss.weld.environment.servlet.Listener());
-        server.setHandler(context);
         server.start();
 
         assertThat(Resource.withUrl("http://localhost:8888/ping"), equalTo("42isTheAnswer"));
---
+++ src/test/java/hub/support/RouteTest.java
@@ -0,0 +1,31 @@
+package hub.support;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.After;
+import org.junit.Before;
+
+public class RouteTest {
+
+    protected Server server;
+    protected ServletContextHandler context;
+
+    @Before
+    public void aJettyServer() throws Exception {
+        server = new Server(8888);
+        context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.setResourceBase("src/main/resources");
+        context.addEventListener(new org.jboss.weld.environment.servlet.Listener());
+        server.setHandler(context);
+    }
+    @After
+    public void stopServer() throws Exception {
+        server.stop();
+    }
+
+    @Before
+    public void setProperties() {
+        System.setProperty("COA_SEARCH_ENDPOINT", "http4://localhost:8111");
+    }
+}
