62635ac7fc4aa796dde9b8f465aef11d517419bb
Eric Mignot <eric.mignot@gmail.com>
2018-08-09 14:26:24 -0700

    Acknowledge we plan to deploy on undertow 1.4.0.Final

--- src/test/java/hub/ActiveMqServletWithUndertowTest.java
+++ src/test/java/hub/ActiveMqServletWithUndertowTest.java
@@ -1,16 +1,21 @@
 package hub;
 
 import hub.http.ActiveMqServlet;
+import hub.support.AsyncHttpResponse;
 import hub.support.HttpResponse;
 import hub.support.ThirdParty;
 import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import javax.servlet.ServletException;
 
+import static hub.support.AsyncGetRequest.asyncGet;
 import static hub.support.GetRequest.get;
 import static io.undertow.servlet.Servlets.*;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -26,7 +31,8 @@ public class ActiveMqServletWithUndertowTest {
                 .setClassLoader(ActiveMqServletWithUndertowTest.class.getClassLoader())
                 .setContextPath("")
                 .setDeploymentName("test.war")
-                .addServlets(servlet("ActiveMqServlet", new ActiveMqServlet().getClass()).addMapping("/message/*"));
+                .addServlets(servlet("ActiveMqServlet", ActiveMqServlet.class)
+                            .addInitParam("maximumReadTimeout", "3000").addMapping("/message/*"));
         DeploymentManager manager = defaultContainer().addDeployment(servletBuilder);
         manager.deploy();
         HttpHandler servletHandler = manager.start();
@@ -42,16 +48,27 @@ public class ActiveMqServletWithUndertowTest {
     }
 
     @Test
-    @Ignore
-    public void allowsToReadTheQueueTwice() throws Exception {
-        ThirdParty.post("http://localhost:8080/message/this-queue?type=queue", "body=hello");
-
-        HttpResponse response = get("http://localhost:8080/message/this-queue?type=queue");
+    public void exposesQueueForRead() throws Exception {
+        ThirdParty.post("http://localhost:8080/message/hello-queue?type=queue", "body=hello");
+        HttpResponse response = get("http://localhost:8080/message/hello-queue?type=queue");
+        
         assertThat(response.getStatusCode(), equalTo(200));
         assertThat(response.getBody(), equalTo("hello"));
+    }
 
-        response = get("http://localhost:8080/message/this-queue?type=queue");
+    @Test
+    public void returnsNoContentAfterTimeoutWhenQueueIsEmpty() throws Exception {
+        HttpResponse response = get("http://localhost:8080/message/empty-queue?type=queue");
+
         assertThat(response.getStatusCode(), equalTo(204));
         assertThat(response.getBody(), equalTo(""));
+    }
+
+    @Test
+    public void readCanWaitForNewMessage() throws Exception {
+        AsyncHttpResponse client = asyncGet("http://localhost:8080/message/this-queue?type=queue");
+        ThirdParty.post("http://localhost:8080/message/this-queue?type=queue", "body=welcome");
+
+        Assert.assertThat(client.getBody(), equalTo("welcome"));
     }
 }
